a2e6a70b64cabd715d1b6e5efbc90eaf098058df
Rafael Santos <rafaelsantos.rse@gmail.com>
2017-08-23 21:48:36 -0300

    agora foi!

--- 4.1.py
+++ 4.1.py
@@ -1,4 +1,5 @@
 from math import sqrt
+
 j = input()
 i = 0
 while i < j:
@@ -6,24 +7,31 @@ while i < j:
     n = input()
     x = int(sqrt(n))
     y = 0
+    c = 'N'
     if (x*x == n):
-        print("{} {} {}".format(x, y, 'S'))
+        c = 'S'
+        print("{} {} {}".format(x, y, c))
         break
     else:
-        print("{} {} {}".format(x, y, 'N'))
+        print("{} {} {}".format(x, y, c))
         x += 1
         while x < (n+1)/2:
             y = sqrt(x*x - n)
             if (y - int(y) == 0.0) :
-                print("{} {} {}".format(x, int(y), 'S'))
+                c = 'S'
+                print("{} {} {}".format(x, int(y), c))
                 break
             else:
-                print("{} {} {}".format(x, int(y), 'N'))
+                print("{} {} {}".format(x, int(y), c))
                 x += 1
-                
-    a = int(x + y)
-    b = int(x - y)
+    if (c == 'S'):
+        
+        a = int(x + y)
+        b = int(x - y)
+    else:
+        a = n
+        b = 1
     print("{} {}".format(b, a))
     print("---")
     i+=1
-    
+
